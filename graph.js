b548f13679cacfa772fff66257c9f65aa106bf97
Toitle419 <117880942+Toitle419@users.noreply.github.com>
2024-08-29 20:05:25 +1000

    Added chart and changed outlines to borders

---
+++ graph.js
@@ -0,0 +1,34 @@
+const ctx = document.getElementById('graph-canvas');
+
+const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+const values = new Array(months.length);
+for (var i = 0; i < values.length; i++)
+    {
+        values[i] = Math.random() * 100
+    };
+    
+var chart = new Chart(ctx,
+    {
+    type: 'line',
+    data:
+    {
+        labels: months,
+        datasets:
+        [{
+            label: 'Value',
+            data: values,
+            borderWidth: 1
+        }]
+    },
+    options:
+    {
+        scales:
+        {
+            y:
+            {
+                beginAtZero: true
+            }
+        },
+        animation: false
+    }
+});
