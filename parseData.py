11db5079ff5a1cd36adac592ed638b41edcd95a2
Toitle419 <117880942+Toitle419@users.noreply.github.com>
2024-08-30 10:42:07 +1000

    Data parsing
    Added parseData.py
    This file parses the BTC historical data to a file with only start date and open price

--- parseData.py
+++ parseData.py
@@ -1,2 +1,16 @@
 import csv, os
+import numpy as np
 
+data = np.genfromtxt('bitcoin_2010-07-17_2024-08-29.csv', encoding='utf-8-sig', dtype=None, delimiter=',', names=True)
+
+
+dates = data['Start']
+opens = data['Open']
+
+outData = np.flip(np.stack((dates, opens), 1), 0)
+print(outData)
+
+with open("dates_opens.csv", 'w', newline='') as outfile:
+    writer = csv.writer(outfile, delimiter=',')
+    writer.writerow(('Start', 'Open'))
+    writer.writerows(outData)
